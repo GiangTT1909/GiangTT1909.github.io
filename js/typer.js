28e6c6a45be0c400128d534904aa15a2f7ff73d2
GiangTT09 <64791934+GiangTT09@users.noreply.github.com>
2020-06-14 02:34:13 +0700

    web

---
+++ js/typer.js
@@ -0,0 +1,36 @@
+/*
+
+Template: Profyl - Personal Vcard Resume HTML Template
+Author: iqonicthemes.in
+Version: 1.0
+
+*/
+
+/*----------------------------------------------
+Index Of Script
+------------------------------------------------
+
+1.Page Loader
+2.Portfolio isotope
+3.NiceScroll
+4.Progress Bar
+5.Magnific Popup
+6.Typer
+7.Owl Carousel
+
+------------------------------------------------
+Index Of Script
+----------------------------------------------*/
+
+$(document).ready(function() {
+
+
+
+    /*------------------------
+    Typer
+    --------------------------*/
+    var win = $(window),
+        foo = $('#typer');
+    foo.typer(['<h1><span class="iq-font-purple">Web</span> Developer</h1>', '<h1>Web <span class="iq-font-purple">Designer</span></h1>', '<h1><span class="iq-font-purple">Frontend</span> Developer</h1>']);
+
+});
